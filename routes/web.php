8766b86738b26859a6bec36804dcc39ebdc263be
Eugene Lobach <ucta456@gmail.com>
2022-06-13 12:06:18 +0300

    Support react router

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('{reactRoutes}', function () {
+    return view('welcome');
+})->where('reactRoutes', '^((?!api).)*$');
