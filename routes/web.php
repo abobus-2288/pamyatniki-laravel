3b3a71212a41abaf326bf0578bdb8ef25365ba30
Eugene Lobach <ucta456@gmail.com>
2022-06-17 16:44:31 +0300

    Routes

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::redirect('/public', '/');
 
 Route::get('{reactRoutes}', function () {
     return view('welcome');
