71ad9a0a7731260c0d0b372ad547840f09e9f5b3
Eugene Lobach <ucta456@gmail.com>
2022-06-20 15:32:13 +0300

    test api route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +14,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/users', function () {
+    $users = User::all();
+
+    return response()->json($users);
+});
